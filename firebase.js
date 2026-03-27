9c94cfc987410ac3a55c531f648663bd495acc20
cochlearkar <cochlearkar@gmail.com>
2026-03-27 22:27:04 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,9 +2,9 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
+  apiKey: "AIzaSyAlfhXKGJyVJ5NiolhEJQ8aIfp27xScewM",
+  authDomain: "task-manager-app-86f59.firebaseapp.com",
+  projectId: "task-manager-app-86f59",
 };
 
 const app = initializeApp(firebaseConfig);
